e024a84ab5198d23dc2ccc05518f267900693d3e
MountVesuvius <JudeALakkis@gmail.com>
2023-01-03 22:55:42 +0100

    work in progress

--- src/App.js
+++ src/App.js
@@ -8,18 +8,25 @@ const gliderSprites = ["Hylian Kite.png", "Peach Parasol.png", "Parachute.png", 
 const kartSprites = ["Flame Rider.png", "Streetle.png", "B Dasher.png", "Circuit Special.png", "City Tripper.png", "Gold Standard.png", "Koopa Clown.png", "Bone Rattler.png", "Jet Bike.png", "Steel Driver.png", "P Wing.png", "Prancer.png", "Mr Scooty.png", "Biddybuggy.png", "Wild Wiggler.png", "Inkstriker.png", "Tanooki Kart.png", "Blue Falcon.png", "Teddy Buggy.png", "Standard Kart.png", "Standard Bike.png", "Sports Coupe.png", "W 25 Silver Arrow.png", "Splat Buggy.png", "300 SL Roadster.png", "Yoshi Bike.png", "Comet.png", "GLA.png", "Master Cycle.png", "Master Cycle Zero.png", "Sport Bike.png", "Pipe Frame.png", "Cat Cruiser.png", "Badwagon.png", "Tri Speeder.png", "Sneaker.png", "Standard ATV.png", "Varmint.png", "Landship.png", "Mach 8.png", "The Duke.png", ]
 const tireSprites = ["Gold Tires.png", "Retro Off Road.png", "Metal.png", "Azure Roller.png", "Standard.png", "Blue Standard.png", "Sponge.png", "Roller.png", "Slick.png", "Wood.png", "Hot Monster.png", "Cyber Slick.png", "Off Road.png", "GLA Tires.png", "Leaf Tires.png", "Triforce Tires.png", "Ancient.png", "Slim.png", "Monster.png", "Button.png", "Crimson Slim.png", "Cushion.png", ]
 
+const imgDir = "./Sprites/"
+
 function App() {
   return (
       <>
         <div className="slot">
         <Carousel>
           <CarouselItem>Item 1</CarouselItem>
-          {/* {charSprites.forEach((x,i) => <CarouselItem>x</CarouselItem>)} */}
-          {/* <CarouselItem>Item 2</CarouselItem> */}
-          {/* <CarouselItem>Item 3</CarouselItem> */}
-          {/* <CarouselItem>Item 4</CarouselItem> */}
-          {/* <CarouselItem>Item 5</CarouselItem> */}
-          {/* <CarouselItem>{charSprites[1]}</CarouselItem> */}
+          {/* Not sure why this isn't working yet */}
+          {
+            charSprites.map((item, index) => {
+              return (
+                <CarouselItem>
+                <img src={imgDir + item} alt={item} id={index}/>
+                </CarouselItem>
+
+                );
+            })
+          }
         </Carousel>
 
         </div>
