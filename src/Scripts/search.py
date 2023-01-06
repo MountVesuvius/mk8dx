b1e2f2ed45d1646638f82ebae7f1db75e37515b2
MountVesuvius <JudeALakkis@gmail.com>
2023-01-06 23:54:19 +0100

    figuring out how to speed up combination searches for part groups

--- src/Scripts/search.py
+++ src/Scripts/search.py
@@ -11,39 +11,71 @@ Output:
 input:
 - stat (string)
 - value (int)
+
+
+creating a json file with breakdowns for each stat
+then you can reloop through them
+
 '''
 
-def foo(stat, value):
+'''
+Groups all four parts into segments based on a single statistic.
+This alls for overall stat calculations within ranges to be done
+much faster, as only single values need to be compared and
+combined rather than checking every part in the game.
+
+These will be turned into static json files for faster lookup times.
+'''
+def statGrouping(stat):
 	groups = {
 		'char': {},
 		'body': {},
 		'tire': {},
 		'glider': {}
 	}
+
 	for group in groups:
 		for obj in data['part_dict'][group]:
 			if obj[stat] not in groups[group]:
 				groups[group][obj[stat]] = []
 			groups[group][obj[stat]].append(obj['name'])
 
-	# for i in data['part_dict']['body']:
-	# 	if i[stat] not in dic:
-	# 		dic[i[stat]] = []
-	# 	dic[i[stat]].append(i['name'])
-	# print(dic)
-	# glider = [(i[stat], i['name']) for i in data['part_dict']['glider']]
-	for i in groups['glider']:
-		print(i, groups['glider'][i])
+	return groups
 
+'''
+four values must sum to equal `value`
+first value is for the character, so the user selects their character
+independently from the body, tire, and glider. this was done to 
+reduce computation time.
 
+Can probably optimise this, but i've cut it down a lot already.
+Speed will come from reading the JSON rather than computing the groups
+'''
+def find_combinations(val, l1, l2, l3, target_sum):
+  combinations = []
+  for i in l1:
+    for j in l2:
+      k = target_sum - val - i - j
+      if k in l3:
+        combinations.append((val, i, j, k))
+  return combinations
 
-
-
+def getStatBuild(groups, stat, value):
+	a = [list(groups[group]) for group in groups]
+	b = find_combinations(10, a[1], a[2], a[3], value)
+	print(b)
 
 f = open(file)
 data = json.load(f);
+
+weight = statGrouping('wgt')
+getStatBuild(weight, 0, 15)
+
 # print(data)
-foo('wgt', 6)
+# for i in data['attr_keys']:
+# 	print(data['attr_keys'][i])
+# 	foo(i)
+# 	print('\n')
 
 
 f.close()
