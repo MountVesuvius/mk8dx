d8ac19cdde880bba405289c06d7fd345a379aadf
MountVesuvius <JudeALakkis@gmail.com>
2023-01-09 22:45:44 +0100

    testing

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,15 @@
 import react from 'react';
 
+import StatBar from '../Components/StatBar';
+
 const Home = ({}) => {
 	return (
 		<>
-		<h1>Script test</h1>
-		<script src="../Scripts/search.js"></script>
+		<h1>Component Testing</h1>
+		<StatBar title={"help"} points={[15,5,7]}></StatBar>
+		<StatBar title={"yikes"} points={[15,5,7,8,20,5]}></StatBar>
+		<StatBar title={"wow"} points={1}></StatBar>
+		<StatBar title={"wow"} points={2}></StatBar>
 		</>
 	)
 }
