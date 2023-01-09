af5e827c7cd070edbd580405007b6f2f32c0b8ff
MountVesuvius <JudeALakkis@gmail.com>
2023-01-09 22:45:04 +0100

    multibars

--- src/Components/StatBar.jsx
+++ src/Components/StatBar.jsx
@@ -2,32 +2,32 @@ import react from 'react';
 
 import '../Styles/StatBar.css';
 
-/*
-TODO:
-Create internal bar stacking option to group like stats
-- Traction On/Off Road 2 Bars
-- Terrain Speed 4 Bars
-- Terrain Handling 4 Bars
+const StatBar = ({title, points}) => {
 
-Can do this by dynamically adding `filler` divs and 
-changing their input based on an array. Will probably
-need to change how `points` works…
+	const barList = [];
 
-*/
+	if (points.constructor != Array) {
+		points = [points];
+	}
 
-const StatBar = ({ title, points}) => {
-  return (
-  	<>
-		<span>{title}</span>
-		<div className="bar-container">
-		    <div className="bar-wrapper">
-		      <div className="filler" style={{width: `${(points/20)*100}%`}}>
-		        <span className="label">{`${points}`}</span>
-		      </div>
-		    </div>
-		</div>
-    </>
-  );
-};
+	for (let i = 0; i < points.length; i++) {
+		barList.push(
+				<div className="filler" style={{width: `${(points[i]/20)*100}%`}}>
+					<span className="label">{points[i]}</span>	
+				</div>
+			);
+	}
+
+	return (
+		<>
+			<span>{title}</span>
+			<div className="bar-container">
+				<div className="bar-wrapper">
+					{barList}	
+				</div>
+			</div>
+		</>
+	)
+}
 
 export default StatBar;
