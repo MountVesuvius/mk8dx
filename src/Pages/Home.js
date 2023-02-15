d31f12b16fd63a0873009395523a3139dac18523
MountVesuvius <JudeALakkis@gmail.com>
2023-02-16 08:17:17 +1100

    testing changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,15 +1,19 @@
 import react from 'react';
 
 import StatBar from '../Components/StatBar';
+import Chart from '../Components/Chart';
+import BarChart from '../Components/BarChart';
 
 const Home = ({}) => {
 	return (
 		<>
 		<h1>Component Testing</h1>
-		<StatBar title={"help"} points={[15,5,7]}></StatBar>
-		<StatBar title={"yikes"} points={[15,5,7,8,20,5]}></StatBar>
-		<StatBar title={"wow"} points={1}></StatBar>
-		<StatBar title={"wow"} points={2}></StatBar>
+		{/* <StatBar title={"help"} points={[15,5,7]}></StatBar> */}
+		{/* <StatBar title={"yikes"} points={[15,5,7,8,20,5]}></StatBar> */}
+		{/* <StatBar title={"wow"} points={1}></StatBar> */}
+		{/* <StatBar title={"wow"} points={2}></StatBar> */}
+		<Chart></Chart>
+		{/* <BarChart/> */}
 		</>
 	)
 }
